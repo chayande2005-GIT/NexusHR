23333a1fa03f4ffbbc6070cf0b06cd07bfa4dc70
chayande2005-GIT <chayande2005@gmail.com>
2026-04-26 00:21:50 +0530

    AttendanceRepository & LeaveRepository Built

---
+++ src/main/java/com/nexushr/nexushr/repository/AttendanceRepository.java
@@ -0,0 +1,13 @@
+package com.nexushr.nexushr.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.nexushr.nexushr.entity.Attendance;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+	Optional<Attendance> findTopByEmployeeIdOrderByCheckInDesc(Long employeeId);
+	Optional<Attendance> findByEmployeeIdAndDate(Long employeeId, LocalDate date);
+}
