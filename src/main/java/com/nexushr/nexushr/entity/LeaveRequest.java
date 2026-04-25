9fb988e9d0f9dc296364a23b1fc298699fa9923c
chayande2005-GIT <chayande2005@gmail.com>
2026-04-26 00:20:08 +0530

    Attendance & Leave Request Entity Created

---
+++ src/main/java/com/nexushr/nexushr/entity/LeaveRequest.java
@@ -0,0 +1,26 @@
+package com.nexushr.nexushr.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "leave_requests")
+public class LeaveRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fromDate;
+    private LocalDate toDate;
+
+    private String reason;
+
+    private String status; // PENDING, APPROVED, REJECTED
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+}
